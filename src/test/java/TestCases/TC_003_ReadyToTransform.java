76abbbc36d6c031c042dd5d0c3bb15d39c98827d
2318454 <2318454@LTIN486385.cts.com>
2024-04-07 12:51:01 +0530

    aditya logs added

--- src/test/java/TestCases/TC_003_ReadyToTransform.java
+++ src/test/java/TestCases/TC_003_ReadyToTransform.java
@@ -17,23 +17,30 @@ public class TC_003_ReadyToTransform extends BaseClass{
 	public void navigationToContactUsPage() throws InterruptedException{
 		ro=new ReadytotransformObject(driver,mywait);
 		ro.ForIndividualsClick();
+		log.info("user scrolling the page to the bottom");
 		ro.scrolldown();
 		ro.ForEnterprise();
+		log.info("User clicked on ForEnterprise");
 		ro.SolutionClick();
+		log.info("Users clicks on the solutions");
 		ro.Courses();
 		ro.Contactclick();
+		log.info("users clicks on the ready to transform");
 }
 
 	@Test(priority=2)
 	
 	public void fillingTheForm() throws IOException {
 		Rf=new RegisterForm(driver,mywait);
+		log.info("User navigated to ready to transform page");
+		log.info("user entered all the detailed");
 		Rf.FirstName();
 		Rf.LatName();
 		Rf.email();
 		Rf.PhoneNo();
 		Rf.Submitbutton();
 		
+		log.info("User captured the error message");
 		String ErrorMess=Rf.Error();
 		System.out.printf("ERROR MESSAGE:");
 		eu.setCellData(rowNum, 0, "Error Message", 0);
